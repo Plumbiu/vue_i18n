8e428d884702a5eab18a292caf51d8851a0b4cc5
Plumbiu <99574369+Plumbiu@users.noreply.github.com>
2023-01-30 16:18:40 +0800

    first commit

---
+++ i18n/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { createI18n } from 'vue-i18n'
+const app = createApp(App)
+const i18n = createI18n({
+
+})
+app.use(i18n)
+app.mount('#app')
